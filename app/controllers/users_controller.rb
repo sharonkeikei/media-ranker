01b2fee45c53b2291275b94841a71698ee15ac7e
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-17 22:24:13 -0700

    added all users controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,41 @@ class UsersController < ApplicationController
   def show
     check_user
   end
+  
+  def login_form
+    @user = User.new
+  end
+
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as existing user #{username}"
+    else
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{username}"
+    end
+
+    redirect_to root_path
+  end
+
+  def logout
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      unless user.nil?
+        flash[:notice] = "Goodbye #{user.username}"
+        session[:user_id] = nil
+      else 
+        flash[:notice] = "Error -- unknown user"
+        session[:user_id] = nil
+      end
+    else 
+      flash[:error] = "You must be loggined in before to logout"
+    end
+    redirect_to root_path
+  end
 
   private
 
