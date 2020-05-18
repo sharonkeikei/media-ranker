56603496399834c1167c824deb3c2f0956591f62
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-17 20:58:24 -0700

    added find_top_ten / spotlight methods for Work.rb

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,17 @@
 class Work < ApplicationRecord
   has_many :votes, dependent: :destroy
-  validates :title, presence: true, uniqueness: true
+  validates :title, :category, presence: true, uniqueness: true
+
+  def self.find_top_ten(category)
+    work_by_category = Work.where(category: category)
+    return work_by_category.limit(10).sort_by{|work| work.votes.count}.reverse
+  end
+
+  def self.find_spotlight 
+    works = Work.all
+    return works.max_by{|work| work.votes.count}
+  end
+
+  def self.upvote
+  end
 end
