e7c317f785f1cc412cdd94cf29c8cbaa67494b34
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-18 00:36:43 -0700

    added upvote methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :works 
+  resources :works do
+    get '/upvote', to: "votes#upvote", as: 'upvote'
+  end
   resources :users
-  resources :votes
 
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
