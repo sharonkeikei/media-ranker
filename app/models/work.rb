6a3c711876cdb3a381d8910d3bd5802abc4fc1c0
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-18 01:02:08 -0700

    add sort by date method to work

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,6 +3,11 @@ class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :category, presence: true
 
+  def self.sort_by_vote_order
+    work = Work.all
+    return work.sort_by{|work| work.votes.count}.reverse
+  end
+
   def self.find_top_ten(category)
     work_by_category = Work.where(category: category)
     return work_by_category.limit(10).sort_by{|work| work.votes.count}.reverse
@@ -11,8 +16,5 @@ class Work < ApplicationRecord
   def self.find_spotlight 
     works = Work.all
     return works.max_by{|work| work.votes.count}
-  end
-
-  def self.upvote
   end
 end
