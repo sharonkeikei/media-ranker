b32533ab3fb7f9b303af52b5b00b90bd1a7d4a1e
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-20 15:21:10 -0700

    edited flash colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level.to_sym
+    when :notice 
+      "alert-info"
+    when :success 
+      "alert-success"
+    when :error
+      "alert-warning"
+    when :alert
+      "alert-error"
+    when :warning
+      "alert-warning"
+    end
+  end
 end
