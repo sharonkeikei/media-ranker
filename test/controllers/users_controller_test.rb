808ca711c3cdb4b8e536b1f623fe248fcc2ee083
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-19 23:03:46 -0700

    added user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,74 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
-end
+  it 'can get the login form' do 
+    get login_path
+
+    must_respond_with :success
+  end
+
+  describe 'index' do
+    it 'responds to index' do
+      get users_path
+
+      must_respond_with :success
+    end
+  end
+
+  describe 'show' do
+    it 'will get show for a valid user' do
+    
+      get user_path(users(:kitty).id)
+
+      must_respond_with :success
+    end
+
+    it 'will redirect to users path if there is no a valid id provided' do
+      invalid_id = -1
+
+      get user_path(invalid_id)
+
+      must_redirect_to users_path
+    end
+    
+  end
+
+  describe 'logging in' do
+    it 'can login a new uesr' do
+      # using login helper method from test helper
+      user = nil 
+      expect{
+        user = login()
+      }.must_differ "User.count", 1
+
+      must_respond_with :redirect
+
+
+      expect(user).wont_be_nil
+      expect(session[:user_id]).must_equal user.id
+      expect(user.username).must_equal "Gillian Chung"
+    end
+
+    it 'can login an exisiting user' do
+      # using existing user in yml
+      expect{
+        login('Hello Kitty')
+      }.wont_change "User.count"
+      
+      expect(session[:user_id]).must_equal (users(:kitty).id)
+    end
+  end
+
+  describe 'logout' do
+    it 'can logoout a logged in user' do
+      login()
+
+      expect(session[:user_id]).wont_be_nil
+
+      post logout_path
+
+      expect(session[:user_id]).must_be_nil
+    end
+  end
+
+end 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,4 +18,18 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  def login(username = "Gillian Chung")
+    # arrange
+      user_hash = {
+        user: {
+          username: username
+        }
+      }
+      post login_path, params: user_hash
+
+      user = User.find_by(username: username)
+      return user
+  end
+
 end
